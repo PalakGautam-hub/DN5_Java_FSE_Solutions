cd663a293cfbe171164a34af53fb33f710645489
Palak Gautam <gautampalak77@gmail.com>
2026-06-22 16:13:07 +0530

    dsa ques

---
+++ hands-on/dsa/exercise7.java
@@ -0,0 +1,34 @@
+public class exercise7 {
+
+    // Recursive method to calculate future value
+public static double futureValue(double currentValue,
+                                 double growthRate,
+                                 int years) {
+
+    // Base case
+    if (years == 0) {
+        return currentValue;
+    }
+
+    // Recursive call
+    return futureValue(currentValue, growthRate, years - 1)
+            * (1 + growthRate);
+}
+
+    public static void main(String[] args) {
+
+        double currentValue = 10000;
+        double growthRate = 0.10; // 10%
+        int years = 5;
+
+        double result = futureValue(currentValue,
+                                    growthRate,
+                                    years);
+
+        System.out.printf(
+                "Future Value after %d years = %.2f",
+                years,
+                result
+        );
+    }
+}
